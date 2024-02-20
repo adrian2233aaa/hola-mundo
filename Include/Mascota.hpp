b58a815894e80edb2cbe616a67c02d8ca100b12f
adrian2233aaa <160290552+adrian2233aaa@users.noreply.github.com>
2024-02-20 02:00:09 +0000

    Version 1

---
+++ Include/Mascota.hpp
@@ -0,0 +1,16 @@
+class Mascota
+{
+private:
+    int Felicidad;
+    int Energia;
+public:
+    Mascota() {}
+    ~Mascota() {}
+    void Comer(){}
+    this->Energia += 0;
+
+}
+int LeerEnergia()
+    return this->Energia;
+}
+include <alimento>
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[]);
 {
-    /* code */
+    Mascota m1;
+    Mascota m2;
+    Mascota m3;
+
+    m1.comer();
+    m2.comer();
+    m3.comer();
+    cout<<"Mascota"<<m1.LeerEnergia()<<end1
+
     cout<<"Hola"<<23110126<<endl;
     return 0;
+
 }
